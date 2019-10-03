b52cba031471672632b48e52516ea150b8b8d8f4
wholemann <wholeman.dev@gmail.com>
2019-10-03 21:32:53 +0900

    Practice react hooks implemented by TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Greetings from './Greetings';
+import Counter from './Counter';
+import MyForm from './MyForm';
+import ReducerSample from './ReducerSample';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+function App() {
+  const onSubmit = (form: { name: string; description: string }) => {
+    console.log(form);
+  };
+  return <MyForm onSubmit={onSubmit} />;
+};
 
 export default App;
